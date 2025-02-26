f9f0f25115ef54ee6323b150e19ac056d58c132f
KAYLEE_ALEJANDRA_LUEVANO_MOLINA <a.22308051281248@cbtis128.edu.mx>
2025-02-26 20:35:03 +0000

    im

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,92 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MisIconos());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MisIconos extends StatelessWidget {
+  const MisIconos({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Iconos con Subtítulos'), // Título de la AppBar
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Primera fila de iconos con subtítulos
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  IconWithSubtitle(
+                    icon: Icons.home,
+                    subtitle: 'Inicio',
+                    color: Colors.blue,
+                  ),
+                  IconWithSubtitle(
+                    icon: Icons.favorite,
+                    subtitle: 'Favoritos',
+                    color: Colors.red,
+                  ),
+                  IconWithSubtitle(
+                    icon: Icons.thumb_up,
+                    subtitle: 'Like',
+                    color: Colors.pink,
+                  ),
+                ],
+              ),
+              SizedBox(height: 20), // Espacio entre las filas
+              // Segunda fila de iconos con subtítulos
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  IconWithSubtitle(
+                    icon: Icons.star,
+                    subtitle: 'Destacados',
+                    color: Colors.purple,
+                  ),
+                  IconWithSubtitle(
+                    icon: Icons.settings,
+                    subtitle: 'Ajustes',
+                    color: Colors.grey,
+                  ),
+                  IconWithSubtitle(
+                    icon: Icons.thumb_down,
+                    subtitle: 'dislike',
+                    color: Colors.lightBlueAccent,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class IconWithSubtitle extends StatelessWidget {
+  final IconData icon;
+  final String subtitle;
+  final Color color;
+
+  const IconWithSubtitle({
+    required this.icon,
+    required this.subtitle,
+    required this.color,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+    return Column(
+      children: [
+        Icon(icon, size: 50.0, color: color),
+        SizedBox(height: 8), // Espacio entre el icono y el subtítulo
+        Text(subtitle, style: TextStyle(fontSize: 16)),
+      ],
     );
   }
 }
